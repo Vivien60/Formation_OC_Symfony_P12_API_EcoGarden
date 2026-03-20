b5f3ed2ea2e47e4f276df621c6c09549e5dc7832
Vivien60 <phedras.ch@gmail.com>
2026-03-20 12:31:18 +0100

    Retrait entité City

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -32,9 +32,6 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?string $password = null;
 
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $city = null;
-
     public function getId(): ?int
     {
         return $this->id;

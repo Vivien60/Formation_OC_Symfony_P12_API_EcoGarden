241787c1116e5bf7eadbfa9333c04975240cf109
Vivien60 <phedras.ch@gmail.com>
2026-03-20 15:20:20 +0100

    feat: implémentation des routes pour advices -wip
    - Création et implémentation des routes CRUD pour advice
    Note : les mois concernés pour les conseils restent
    à ajouter dans l'impléméntation

--- src/Controller/AdviceController.php
+++ src/Controller/AdviceController.php
@@ -2,12 +2,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Advice;
 use App\Enum\Month;
 use App\Repository\AdviceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use JMS\Serializer\DeserializationContext;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 final class AdviceController extends AbstractController
@@ -33,5 +38,42 @@ final class AdviceController extends AbstractController
         $advices = $adviceRepository->findByMonth($month);
         $advicesSerialized = $serializer->serialize($advices, 'json', ['groups' => 'getAdvices']);
         return new JsonResponse($advicesSerialized, Response::HTTP_OK, [], true);
+    }
+
+    #[Route('/advices/{id}', name: 'advices_update', methods: 'PUT')]
+    public function updateAdvice(Advice $currentAdvice, Request $request, EntityManagerInterface $em, SerializerInterface $serializer): JsonResponse
+    {
+        $requestContent = $request->getContent();
+        $updatedAdvice = $serializer->deserialize(
+            $requestContent,
+            Advice::class,
+            'json',
+            [AbstractNormalizer::OBJECT_TO_POPULATE => $currentAdvice]
+        );
+        $em->persist($updatedAdvice);
+        $em->flush();
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/advices/{id}', name: 'advices_delete', methods: 'DELETE')]
+    public function deleteAdvice(Advice $advice, EntityManagerInterface $em): JsonResponse
+    {
+        $em->remove($advice);
+        $em->flush();
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/advices', name: 'advices_create', methods: 'POST')]
+    public function createAdvice(Request $request, EntityManagerInterface $em, SerializerInterface $serializer): JsonResponse
+    {
+        $requestContent = $request->getContent();
+        $newAdvice = $serializer->deserialize(
+            $requestContent,
+            Advice::class,
+            'json'
+        );
+        $em->persist($newAdvice);
+        $em->flush();
+        return new JsonResponse(null, Response::HTTP_CREATED);
     }
 }
